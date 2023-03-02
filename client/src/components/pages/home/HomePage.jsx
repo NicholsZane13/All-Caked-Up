07cc684f70f13ecbed3cfcaf6b645e2e44cd69b9
nicholsz <nichols.zane16@gmail.com>
2023-03-02 16:39:27 -0600

    tailwind errors executed

--- client/src/components/pages/home/HomePage.jsx
+++ client/src/components/pages/home/HomePage.jsx
@@ -8,17 +8,10 @@ import "../../../index.css";
 
 function HomePage() {
   return (
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>All Caked Up</title>
-    </head>
     <body>
         <Header />
         <Navbar />
-        <div className="bg-green-900 rounded-md">
+        <div className="bg-green-900">
             <h1 className="">Origin Story : All Caked Up</h1>
             <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
             molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
@@ -35,7 +28,6 @@ function HomePage() {
         {/* <Contact /> */}
         <Footer />
     </body>
-    </html>
   );
 }
 

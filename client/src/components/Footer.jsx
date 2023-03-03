aff8627a1f6392512d5fdab3b439859e04d311c8
Riley Rahe <rilrahe@gmail.com>
2023-03-02 19:16:20 -0600

    adding classes to react components

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,16 +6,9 @@ function Footer() {
       <div className="footer-container">
         <div className="left-section">
           <h3>Company Name</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p>🎂All Caked Up🍑</p>
         </div>
-        <div className="right-section">
-          <h3>Contact Us</h3>
-          <ul>
-            <li>Email: info@company.com</li>
-            <li>Phone: (123) 456-7890</li>
-            <li>Address: 123 Main Street, Anytown USA</li>
-          </ul>
-        </div>
+        
       </div>
       <div className="bottom-bar">
         <p>🎂</p>

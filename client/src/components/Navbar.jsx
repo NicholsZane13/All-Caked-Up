5c96a5d563a299fef3f4d91d64b0e49950272134
Riley Rahe <rilrahe@gmail.com>
2023-03-03 19:56:06 -0600

    working navbar, footer, and portfolio

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React from "react";
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contact</a></li>
+    <nav class="sticky top-0 z-50 group">
+      <ul class="hidden group-hover:block">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+        <li>
+          <a href="#">Menu</a>
+        </li>
+        <li>
+          <a href="#">Online Order</a>
+        </li>
       </ul>
     </nav>
   );

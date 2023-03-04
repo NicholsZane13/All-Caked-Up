8409e3a361312761339454b05f61266b19c6d8ea
nicholsz <nichols.zane16@gmail.com>
2023-03-04 16:42:57 -0600

    working services placement

--- client/src/components/pages/services/Services.jsx
+++ client/src/components/pages/services/Services.jsx
@@ -15,29 +15,42 @@ function ServicePage() {
     <body>
         <Header />
         <Navbar />
-        <div className="grid-rows-6 bg-green flex items-center justify-center flex-col text-center pt-20 pb-10">
+        <div className="bg-green justify-center text-center pt-20 pb-10">
             <h1 className="font-Gloock text-tan text-4xl md:text-7xl mb-1 md:mb-3 font-bold pb-5">Services We Provide!</h1>
-                <div className="">
-                    <img className="" src={cakeArt} alt="edible image"></img>
-                    <h3 className="">Wedding Cakes</h3>
+            <div className="grid grid-rows-1 grid-flow-col gap-4">
+                <div class="row-start1 row-end-3 max-w-fit p-2 bg-green border border-gray-200 rounded-lg shadow">
+                <img class="items-center object-contain h-48 w-60 rounded-t-lg" src={cakeArt} alt="edible image" />
+                    <div class="p-1">
+                        <h5 class="mb-2 text-2xl font-bold text-center tracking-tight text-tan">Wedding Cakes</h5>
+                    </div>
                 </div>
-                <div className="">
-                    <img className="" src={cake} alt="edible image"></img>
-                    <h3 className="">Custom Cakes</h3>
+                <div class="row-start1 row-end-3 max-w-fit p-2 bg-green border border-gray-200 rounded-lg shadow">
+                <img class="items-center object-contain h-48 w-60 rounded-t-lg" src={cake} alt="edible image" />
+                    <div class="p-1">
+                        <h5 class="mb-2 text-2xl font-bold text-center tracking-tight text-tan ">Custom Cakes</h5>
+                    </div>
                 </div>
-                <div className="">
-                    <img className="" src={cookie} alt="edible image"></img>
-                    <h3 className="">Custom Cookies</h3>
+                <div class="row-start1 row-end-3 max-w-fit p-2 bg-green border border-gray-200 rounded-lg shadow">
+                <img class="items-center object-contain h-48 w-60 rounded-t-lg" src={cookie} alt="edible image" />
+                    <div class="p-1">
+                        <h5 class="mb-2 text-2xl font-bold text-center tracking-tight text-tan ">Custom Cookies</h5>
+                    </div>
                 </div>
-                <div className="">
-                    <img className="" src={cupcake} alt="edible image"></img>
-                    <h3 className="">Custom Cupcakes</h3>
+                <div class="row-start1 row-end-3 max-w-fit p-2 bg-green border border-gray-200 rounded-lg shadow">
+                <img class="items-center object-contain h-48 w-60 rounded-t-lg" src={cupcake} alt="edible image" />
+                <div class="p-1">
+                    <h5 class="mb-2 text-2xl font-bold text-center tracking-tight text-tan ">Custom Cupcakes</h5>
                 </div>
-                <div className="">
-                    <img className="" src={pastries} alt="edible image"></img>
-                    <h3 className="">Pasteries & More!</h3>
+                </div>
+                <div class="row-start1 row-end-3 max-w-fit p-2 bg-green border border-gray-200 rounded-lg shadow">
+                <img class="items-center object-contain h-48 w-60 rounded-t-lg" src={pastries} alt="edible image" />
+                    <div class="p-1">
+                        <h5 class="mb-2 text-2xl font-bold text-center tracking-tight text-tan ">Pastries & More!</h5>
+                    </div>
                 </div>
         </div>
+</div>
+
         <div className="">
 
         </div>

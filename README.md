988efccc0389c1573783bbaa025cc19012f2d9b7
nicholsz <nichols.zane16@gmail.com>
2023-03-09 14:51:02 -0600

    mobile view homepage and read me finished

--- README.md
+++ README.md
@@ -1,10 +1,4 @@
-Bakery Website
-Welcome to the Bakery Website! This is a React-based website that provides information about our bakery and allows customers to place orders online.
-
-Features
-
-Dependencies
-This project uses the following dependencies:
+![All Caked Up](./client/src/assets/serviceImages/screenshotAllCakedUp.JPG)
 
 https://allcakedup.herokuapp.com/
 
@@ -15,12 +9,10 @@ https://allcakedup.herokuapp.com/
 ## Description
 
 A home page that introduces the bakery and provides links to the various sections of the website.
-An "About Us" page that describes the history of the bakery and our philosophy.
-A "Catalog" page that displays a list of all our products, including images, descriptions, and prices. Customers can filter the products by category and sort them by name or price.
-A "Order" page that allows customers to add products to their cart and place an order. Customers can select the quantity of each product they wish to order and enter their contact and delivery information. The website will calculate the total cost of the order and display it to the customer before they confirm the order.
-A "Contact Us" page that provides our contact information and a form for customers to send us a message.
-Installation
-To run this website on your local machine, you will need to have Node.js and npm installed.
+A "Menu" page that displays a list of all our products, descriptions, and prices. Customers can filter the products by category.
+Coming Soon => An "Order" page that allows customers to add products to their cart and place an order. Customers can select the quantity of each product they wish to order and enter their contact and delivery information. The website will calculate the total cost of the order and display it to the customer before they confirm the order.
+A "Contact Us" section that provides our contact information.
+A "Portfolio" page that displays a catalog of All Caked Up Products and capabilities to satisfy customers.
 
 ## Table of Contents
 
@@ -31,6 +23,7 @@ To run this website on your local machine, you will need to have Node.js and npm
 
 ## Installation
 
+To run this website on your local machine, you will need to have Node.js and npm installed.
 Clone the repository from GitHub:
 bash
 Copy code
@@ -48,11 +41,11 @@ Open your browser and navigate to http://localhost:3000 to view the website.
 
 ## Usage
 
-Main functionality is to create an
+Main functionality is to create an E-Commerce application for the bakery
 
 ## Credits
 
-Youtube, KU TA's and Instructorm, Google
+Youtube, KU TA's and Instructors, Google
 
 Mitchell:
 measterling13
@@ -86,6 +79,8 @@ Licensed under the [![License: MIT](https://img.shields.io/badge/License-MIT-yel
 
 ## Features
 
+Dependencies
+This project uses the following dependencies:
 React
 React Router
 Bootstrap
--- client/src/components/pages/home/HomePage.jsx
+++ client/src/components/pages/home/HomePage.jsx
@@ -1,42 +1,42 @@
 import React from "react";
 import PicturePortfolio from "../../UI/Picture/PicturePortfolio";
-import Contact from "../../Contact";
-import Footer from "../../Footer";
 import "../../../index.css";
 import MainPic from "../../../assets/homePortfolio/mainHeader.jpg";
 
 function HomePage() {
   return (
     <>
-      <div className="bg-green sm:grid-cols-1 flex pl-10 pt-20 pb-10">
-        <div className="content-left">
-          <h1 className="font-Gloock text-left text-tan text-4xl md:text-7xl mb-1 md:mb-3 font-bold">
-            Origin Story : All Caked Up
-          </h1>
-        </div>
-        <div className="">
-          <img
-            className="border rounded-full h-auto max-w-lg mx-auto"
-            src={MainPic}
-            alt="cute pic"
-          />
-        </div>
-        <div>
-          <p className="font-Indie text-tan text-lg max-w-xl m-6 font-bold">
-            Our mission at All Caked Up is to create a memorable and enjoyable
-            experience for every customer by providing the finest quality
-            coffee, custom cakes, and fresh pastries daily. Our commitment to
-            exceeding customer expectations is at the core of everything we do,
-            and we strive to create a warm and welcoming atmosphere where our
-            customers can indulge in the decadent flavors of our treats. Our
-            founder's journey as a retail cake decorator has evolved into a
-            passion for baking and sharing gourmet pastries with the community.
-            At All Caked Up, we take pride in using only the freshest
-            ingredients and baking everything from scratch to ensure that every
-            bite is a delightful experience. Join us on this sweet journey, and
-            let us satisfy your cravings with our exceptional products and
-            exceptional service.
-          </p>
+      <div className=" sm:sm:p-auto flex bg-green flex pl-10 pt-20 pb-10">
+        <div className="sm:grid grid-cols-1 lg:grid grid-cols-3">
+          <div className="">
+            <h1 className="font-Gloock sm:text-2xl text-left text-tan text-4xl md:text-7xl mb-1 md:mb-3 font-bold">
+              Origin Story : All Caked Up
+            </h1>
+          </div>
+          <div className=" lg:px-40">
+            <img
+              className="border rounded-full h-auto max-w-lg mx-auto"
+              src={MainPic}
+              alt="cute pic"
+            />
+          </div>
+          <div>
+            <p className="font-Indie text-right text-tan sm:text-lg lg:text-xl max-w-xl m-6 font-bold">
+              Our mission at All Caked Up is to create a memorable and enjoyable
+              experience for every customer by providing the finest quality
+              coffee, custom cakes, and fresh pastries daily. Our commitment to
+              exceeding customer expectations is at the core of everything we
+              do, and we strive to create a warm and welcoming atmosphere where
+              our customers can indulge in the decadent flavors of our treats.
+              Our founder's journey as a retail cake decorator has evolved into
+              a passion for baking and sharing gourmet pastries with the
+              community. At All Caked Up, we take pride in using only the
+              freshest ingredients and baking everything from scratch to ensure
+              that every bite is a delightful experience. Join us on this sweet
+              journey, and let us satisfy your cravings with our exceptional
+              products and exceptional service.
+            </p>
+          </div>
         </div>
       </div>
 
